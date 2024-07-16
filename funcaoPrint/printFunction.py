a8629c9f0874db362621404ccf9ac7f38e7e522c
gabrielgpertile <gabrielgasperinpertile@gmail.com>
2024-07-16 15:56:10 -0300

    add Use case of the Print() function

---
+++ funcaoPrint/printFunction.py
@@ -0,0 +1,18 @@
+#Imprimindo na tela
+print("Olá, Python!")
+
+#Imprimindo meu nome completo na tela
+print("Gabriel Gasperin Pertile")
+
+#Jeito errado de declara um função print()
+#print(Gabriel)
+
+#Jeito errado de declara um função print()s
+#print"Gabriel"
+
+#Jeito errado de declara um função print()
+#print('Gabriel')
+
+#Python não aceita mais de um instrução na mesma linha de código
+#sem utilizar o ;
+#print("Gabriel Gasperin Pertile") print("Gabriel Gasperin Pertile") 
