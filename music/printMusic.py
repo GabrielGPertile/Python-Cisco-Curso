c808957f6ca651c1fb004abb1b7d4bf5f89d98e9
gabrielgpertile <gabrielgasperinpertile@gmail.com>
2024-07-18 15:33:50 -0300

    Add music code!

---
+++ music/printMusic.py
@@ -0,0 +1,13 @@
+#Imprimindo na tela 
+#Cada execução da função print() cria uma nova linha.
+print("A pequenina aranha\nescalou a tromba d'água.")
+print()
+
+#Por mais que não contenha nenhum argumento na função, 
+#ela ainda produzira uma nova linha, mas não é a unica forma 
+#fazer uma nova linha
+print() 
+
+#Para fazer um nova linha usamos o cara n ao lado da \ 
+#desse jeito \n
+print("Caiu a Chuva \ne lavou a aranha.")
